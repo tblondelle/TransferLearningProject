c2692e5e2ca99a9dd8fa91591da3d30c9e431962
Thomas Blondelle <23367352+tblondelle@users.noreply.github.com>
2018-01-25 11:36:56 +0100

    convert tabs

--- main.py
+++ main.py
@@ -25,47 +25,47 @@ TESTING_SET_FOLDER_2 = ""
 
 
 def stripMetadata(source_folder, target_folder):
-	"""
-	Get files from the folder [source_folder], 
-	Perfom the stripping
-	Put the results in file (or files) in target_folder
-	"""
-	#json_to_text
+    """
+    Get files from the folder [source_folder], 
+    Perfom the stripping
+    Put the results in file (or files) in target_folder
+    """
+    #json_to_text
     pass
 
 def simplifyRatingAndKeepRelevantWords(source_folder, target_folder):
-	"""
-	Get files from the folder [source_folder], 
-	Simplify the rating
-	Put the results in file (or files) in target_folder
-	"""
-	pass
+    """
+    Get files from the folder [source_folder], 
+    Simplify the rating
+    Put the results in file (or files) in target_folder
+    """
+    pass
 
 def createTrainingSetAndTestSet(source_folder, target_training_set_folder, target_testing_set_folder):
-	pass
+    pass
 
 
 
 
 if __name__ == "__main__":
-	## Pre-processing for the two datasets.
-	stripMetadata(ORIGIN_FOLDER_1, STRIPPED_METADATA_FOLDER_1)
-	simplifyRating(STRIPPED_METADATA_FOLDER_1, SIMPLIFIED_RATINGS_FOLDER_1)
-	keepRelevantWords(SIMPLIFIED_RATINGS_FOLDER_1, CLEANED_DATA_FOLDER_1)
+    ## Pre-processing for the two datasets.
+    stripMetadata(ORIGIN_FOLDER_1, STRIPPED_METADATA_FOLDER_1)
+    simplifyRating(STRIPPED_METADATA_FOLDER_1, SIMPLIFIED_RATINGS_FOLDER_1)
+    keepRelevantWords(SIMPLIFIED_RATINGS_FOLDER_1, CLEANED_DATA_FOLDER_1)
 
-	stripMetadata(ORIGIN_FOLDER_2, STRIPPED_METADATA_FOLDER_2)
-	simplifyRating(STRIPPED_METADATA_FOLDER_2, SIMPLIFIED_RATINGS_FOLDER_2)
-	keepRelevantWords(SIMPLIFIED_RATINGS_FOLDER_2, CLEANED_DATA_FOLDER_2)
+    stripMetadata(ORIGIN_FOLDER_2, STRIPPED_METADATA_FOLDER_2)
+    simplifyRating(STRIPPED_METADATA_FOLDER_2, SIMPLIFIED_RATINGS_FOLDER_2)
+    keepRelevantWords(SIMPLIFIED_RATINGS_FOLDER_2, CLEANED_DATA_FOLDER_2)
 
 
-	# Learning
-	createTrainingSetAndTestSet(CLEANED_DATA_FOLDER_1, TRAINING_SET_FOLDER_1, TESTING_SET_FOLDER_1)
-	model1 = learn(TRAINING_SET_FOLDER_1)
-	showResults(model1, TESTING_SET_FOLDER_1)
+    # Learning
+    createTrainingSetAndTestSet(CLEANED_DATA_FOLDER_1, TRAINING_SET_FOLDER_1, TESTING_SET_FOLDER_1)
+    model1 = learn(TRAINING_SET_FOLDER_1)
+    showResults(model1, TESTING_SET_FOLDER_1)
 
-	# TransferLearning
-	createTrainingSetAndTestSet(CLEANED_DATA_FOLDER_2, TRAINING_SET_FOLDER_2, TESTING_SET_FOLDER_2)
-	model2 = transferLearn(model1, TRAINING_SET_FOLDER_2) # <---- Difference here!!
-	showResults(model2, modelTESTING_SET_FOLDER_2)
+    # TransferLearning
+    createTrainingSetAndTestSet(CLEANED_DATA_FOLDER_2, TRAINING_SET_FOLDER_2, TESTING_SET_FOLDER_2)
+    model2 = transferLearn(model1, TRAINING_SET_FOLDER_2) # <---- Difference here!!
+    showResults(model2, modelTESTING_SET_FOLDER_2)
 
 

0578b14473da7553ba1bf3f5950225554a79aabb
T Blondelle <tuili.tb@gmail.com>
2018-01-25 11:31:03 +0100

    finish abstract functions definitions

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
 
+## What means xx_2 and xx_1 ?
+# We will learn xx_2 from xx_1 via transferLearning. 
+
+
+# Origin datafile downloaded from jmcauley.ucsd.edu/data/amazon/ in ORIGIN_FOLDER
+ORIGIN_FOLDER_1 = "" # Books ie
+ORIGIN_FOLDER_2 = "" # Video (we will learn video data from books)
+
+# In this folder are files such that : 
+# - every line is of the form "[original ratings]\t[original review]"
+STRIPPED_METADATA_FOLDER_1 = ""
+STRIPPED_METADATA_FOLDER_2 = ""
+
+# In this folder are files such that : 
+# - every line is of the form "[new ratings]\t[list of relevant words]" with [new ratings] in {0, 1, 2}
+CLEANED_DATA_FOLDER_1 = ""
+CLEANED_DATA_FOLDER_2 = ""
+
+TRAINING_SET_FOLDER_1 = ""
+TESTING_SET_FOLDER_1 = ""
+TRAINING_SET_FOLDER_2 = ""
+TESTING_SET_FOLDER_2 = ""
+
+
 
 def stripMetadata(source_folder, target_folder):
 	"""
@@ -6,9 +30,10 @@ def stripMetadata(source_folder, target_folder):
 	Perfom the stripping
 	Put the results in file (or files) in target_folder
 	"""
+	#json_to_text
     pass
 
-def simplifyRating(source_folder, target_folder):
+def simplifyRatingAndKeepRelevantWords(source_folder, target_folder):
 	"""
 	Get files from the folder [source_folder], 
 	Simplify the rating
@@ -16,37 +41,31 @@ def simplifyRating(source_folder, target_folder):
 	"""
 	pass
 
-
-def keepRelevantWords(source_folder, target_folder):
-	"""
-	Get files from the folder [source_folder], 
-	Keep Relevant word
-	Put the results in file (or files) in target_folder
-	"""
+def createTrainingSetAndTestSet(source_folder, target_training_set_folder, target_testing_set_folder):
 	pass
 
 
 
 
+if __name__ == "__main__":
+	## Pre-processing for the two datasets.
+	stripMetadata(ORIGIN_FOLDER_1, STRIPPED_METADATA_FOLDER_1)
+	simplifyRating(STRIPPED_METADATA_FOLDER_1, SIMPLIFIED_RATINGS_FOLDER_1)
+	keepRelevantWords(SIMPLIFIED_RATINGS_FOLDER_1, CLEANED_DATA_FOLDER_1)
+
+	stripMetadata(ORIGIN_FOLDER_2, STRIPPED_METADATA_FOLDER_2)
+	simplifyRating(STRIPPED_METADATA_FOLDER_2, SIMPLIFIED_RATINGS_FOLDER_2)
+	keepRelevantWords(SIMPLIFIED_RATINGS_FOLDER_2, CLEANED_DATA_FOLDER_2)
 
 
+	# Learning
+	createTrainingSetAndTestSet(CLEANED_DATA_FOLDER_1, TRAINING_SET_FOLDER_1, TESTING_SET_FOLDER_1)
+	model1 = learn(TRAINING_SET_FOLDER_1)
+	showResults(model1, TESTING_SET_FOLDER_1)
 
-# Origin datafile downloaded from jmcauley.ucsd.edu/data/amazon/ in ORIGIN_FOLDER
-ORIGIN_FOLDER = ""
-
-# In this folder are files such that : 
-# - every line is of the form "[original ratings]\t[original review]"
-STRIPPED_METADATA_FOLDER = ""
-
-# In this folder are files such that : 
-# - every line is of the form "[new ratings]\t[original review]" with [new ratings] in {0, 1, 2}
-SIMPLIFIED_RATINGS_FOLDER = ""
+	# TransferLearning
+	createTrainingSetAndTestSet(CLEANED_DATA_FOLDER_2, TRAINING_SET_FOLDER_2, TESTING_SET_FOLDER_2)
+	model2 = transferLearn(model1, TRAINING_SET_FOLDER_2) # <---- Difference here!!
+	showResults(model2, modelTESTING_SET_FOLDER_2)
 
 
-# In this folder are files such that : 
-# - every line is of the form "[new ratings]\t[list of relevant words]"
-KEPT_RELEVANT_WORDS_FOLDER = ""
-
-stripMetadata(ORIGIN_FOLDER, STRIPPED_METADATA_FOLDER)
-simplifyRating(STRIPPED_METADATA_FOLDER, SIMPLIFIED_RATINGS_FOLDER)
-keepRelevantWords(SIMPLIFIED_RATINGS_FOLDER, KEPT_RELEVANT_WORDS_FOLDER)

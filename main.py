b439875bb14028ee658e2a99234477b396467674
Thomas Blondelle <23367352+tblondelle@users.noreply.github.com>
2018-03-03 13:35:10 +0100

    HEAVY MODIFS integrate word2vec model

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 import os
 
 from data.scripts import json_to_text, cleaner, createDatasets
-import learning.Classifiers as clf
+import learning.sklearnClassifier as sklearnClassifier
+import learning.word2vecClassifier as word2vecClassifier
+
+
+# Origin datafile downloaded from jmcauley.ucsd.edu/data/amazon/ in ORIGIN_FOLDER_1
+ORIGIN_FOLDER_1 = "../data/data_books"
+ORIGIN_FOLDER_2 = "../data/data_video" # eg (we will learn video data from books)
+
+# In this folder are files such that : 
+# - every line is of the form "[original ratings]\t[original review]"
+STRIPPED_METADATA_FOLDER_1 = "data/data_books_stripped"
+STRIPPED_METADATA_FOLDER_2 = "data/data_videos_stripped"
+
+# In this folder are files such that : 
+# - every line is of the form "[new ratings]\t[list of relevant words]" with [new ratings] in {"Negative", "Neutral", "Positive"}
+CLEANED_DATA_FOLDER_1 = "../data/data_books_cleaned"
+CLEANED_DATA_FOLDER_2 = "../data/data_videos_cleaned"
+
+TRAINING_SET_FOLDER_1 = "../data/data_books_training_set"
+TESTING_SET_FOLDER_1 = "../data/data_books_testing_set"
+TRAINING_SET_FOLDER_2 = "../data/data_videos_training_set"
+TESTING_SET_FOLDER_2 = "../data/data_videos_testing_set"
+
 
 
 def stripMetadata(source_folder, target_folder):
@@ -33,22 +55,38 @@ def learn(training_set_folder):
     print("|        TRAIN         |")
     print("========================")
 
-    return clf.learn(training_set_folder, dataBalancing=True)
+    sklearn_classifier = sklearnClassifier.MetaClassifier(validation_rate=0.1, n_features=150)
+    sklearn_classifier.train(TRAINING_SET_FOLDER_1, dataBalancing=True)
+
+    word2vec_classifier = word2vecClassifier.W2V(20,5)
+    word2vec_classifier.train(TRAINING_SET_FOLDER_1, dataBalancing=True)
+
+    return [sklearn_classifier, word2vec_classifier]
 
 
 
-def transferLearn(old_model, training_set_folder):
-    new_model = old_model
-    # Change the identity function to something more complex.
-    return new_model
+def transferLearn(old_models, training_set_folder):
+    new_models = []
+
+    for old_model in old_models:
+        if old_model.name == "sklearn_classifier":
+            new_models.append(old_model)
+        if old_model.name == "word2vec_classifier":
+            old_model.train(training_set_folder, dataBalancing=True)
+            new_models.append(old_model)
+
+    return new_models
 
 
 
-def showResults(model, testing_set_folder):
+def showResults(models, testing_set_folder):
     print("========================")
     print("|        TEST          |")
     print("========================")
-    clf.showResults(model, testing_set_folder)
+
+    for model in models:
+        print("\n\n" + model.name)
+        model.showResults(testing_set_folder)
 
 
 def main(origin_folder_1=ORIGIN_FOLDER_1, origin_folder_2=ORIGIN_FOLDER_2, 
@@ -74,7 +112,7 @@ def main(origin_folder_1=ORIGIN_FOLDER_1, origin_folder_2=ORIGIN_FOLDER_2,
     print("##   LEARNING FROM DATASET 1  ##")
     print("##                            ##")
     print("################################")
-    createTrainingSetAndTestSet(cleaned_data_folder_1, training_set_folder_1, testing_set_folder_1)
+    #createTrainingSetAndTestSet(cleaned_data_folder_1, training_set_folder_1, testing_set_folder_1)
     
     model1 = learn(training_set_folder_1)
     showResults(model1, testing_set_folder_1)

192ced31fb4c63ffb5223460ff7e1d2a954d85bb
Hugues-Moreau <hugues.moreau@ecl15.ec-lyon.fr>
2018-03-08 09:34:38 +0100

    ajout de commentaires

--- learning/MLP_base.py
+++ learning/MLP_base.py
@@ -12,7 +12,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import TruncatedSVD
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-import Classifiers
+
 from numpy.random import permutation
 
 import torch
@@ -62,6 +62,10 @@ class my_RNN(nn.Module):
 
 
     def forward(self, input):
+        # Entrées :
+        #   input (variable(mat)) : les instances
+        # Sortie
+        #   Variable(vect) : les prédictions
         X_int_1 = F.relu(self.linear1(input))
         X_int_2 = F.relu(self.linear2(X_int_1))
         return torch.tanh(self.linear3(X_int_2))
@@ -71,23 +75,21 @@ class my_RNN(nn.Module):
 
 
     def train_once(self, input_variable, target_variable,  optimizer, criterion):
+        # Réalise l'entraînement pour une seule epoch
+        # Entrées :
+        #   - n_epochs (int) : nombre de fois qu'on applique toutes les instance de l'ensemble d'apprentissage
+        #   - input_variable Variable(mat) :  instances d'apprentissage
+        #   - target_variable Variable(vect(+1|-1))) : labels
+        #   - optimizer (pytorch object) : le résultat de optim.SGD ou optim.Adam
+        #   - criterion (pytorch object) : le résultat de nn.L1Loss ou nn.MSELoss
+        # Sorties :
+        #   none
         optimizer.zero_grad()
         input_length = input_variable.size()[0]
 
-        loss = 0
-
         output= self(input_variable)
 
-
-        loss += criterion(output.view(1,-1), target_variable.view(-1))
-
-
-            #print(output.data[0,0])
-            #print(target_variable)
-
-            #print(loss.data[0])
-            #print('--')
-
+        loss = criterion(output.view(1,-1), target_variable.view(-1))
         loss.backward()
 
         optimizer.step()
@@ -96,22 +98,31 @@ class my_RNN(nn.Module):
 
 
 
-    def trainIters(self, n_epochs, training_pairs, learning_rate,  print_every=1000, eval_every = 1000):
+    def trainIters(self, n_epochs, training_pairs, te_pairs, learning_rate,  print_every=1000, eval_every = 1000):
+        # Réalise l'entraînement complet, à partir des ensembles d'apprentissage
+        # Entrées :
+        #   - n_epochs (int) : nombre de fois qu'on applique toutes les instance de l'ensemble d'apprentissage
+        #   - training_pairs (Variable(mat), Variable(vect(+1|-1))) ) : instances d'apprentissage
+        #   - te_pairs (list of (Variable(vect), Variable(+1|-1))) : instances de test
+        #   - learning_rate (float) : devine ;)
+        #   - print_every (int) : imprime l'erreur moyenne toutes les print_every epochs
+        #   - eval_every (int) : teste le NN sur la base de test et imprime la matrice de confusion
+        # Sorties :
+        #   none
+
         start = time.time()
         plot_losses = []
         print_loss_total = 0  # Reset every print_every
 
         optimizer = optim.SGD(self.parameters(), lr=learning_rate)
+        # Autre choix possible :
+        #optimizer = optim.Adam(self.parameters(), lr=learning_rate)
 
-        #training_pairs = [variablesFromPair(random.choice(pairs))for i in range(n_epochs)]
 
-        #criterion = nn.CrossEntropyLoss()
-        #criterion = nn.NLLLoss()
         criterion = nn.L1Loss()
         #criterion = nn.MSELoss()
 
         for epoch in range(1, n_epochs + 1):
-            #training_pair = training_pairs[epoch - 1]
 
             input_variable = training_pairs[0]
             target_variable = training_pairs[1]
@@ -135,10 +146,9 @@ class my_RNN(nn.Module):
 
 
     def evaluateRandomly(self, pairs):
-        # evaluate on all pairs, return success rate
+        # evaluate on all pairs, print the confusion matrix
         n_successes = 0
         n_pos = 0 # also computes the proportion of positive reviews
-        #pairs = pairs[:1000]
 
         TP,TN,FP,FN = 0,0,0,0
         for pair in pairs:  # replace with pairs[:n] for testing
@@ -154,7 +164,6 @@ class my_RNN(nn.Module):
                     TP += 1
                 else:
                     TN += 1
-
             else:
                 if note>0:
                     FP += 1
@@ -199,6 +208,16 @@ def getData(folder):
 
 
 def folder2data(train_filename,test_filename,balanced_tr ,balanced_te, n_features):
+    # Entrées :
+    #   - train_filename (str) : le nom du **dossier** (et pas le nom du fichier) où se trouvent les instances d'apprentissage
+    #   - test_filename (str) : le nom du **dossier** (et pas le nom du fichier) où se trouvent les instances de test
+    #   - balanced_tr (bool) : True si l'ensemble d'apprentissage est équilibré; False s'il est laissé tel quel
+    #   - balanced_te (bool) : True si l'ensemble de test est équilibré; False s'il est laissé tel quel
+    #   - n_features (int) : nombre de variables pour coder chaque instance
+    # Sorties :
+    #   - cuple (new_tr_pairs, new_te_pairs):
+    #       new_tr_pairs :  (Variable(mat), Variable(vect(+1|-1))) )
+    #       new_te_pairs : (list of (Variable(vect), Variable(+1|-1)))
 
     tr_te_pairs = {}
 
@@ -208,11 +227,13 @@ def folder2data(train_filename,test_filename,balanced_tr ,balanced_te, n_feature
 
 
     if balanced_tr :
-        pairs_using_numbers = [(-1,text)  for (target,text) in pairs  if target == 'Negative']
+        #Pour un équilibrage 75/25
+        pairs_using_numbers = [(-1,text)  for (target,text) in pairs  if (target == 'Negative' or target == 'Neutral')]
         Positive_reviews =  [(1,text) for (target,text) in pairs if target == 'Positive']
         pairs_using_numbers += Positive_reviews[:int(len(pairs_using_numbers)*3)]
         tr_pairs = pairs_using_numbers
         """
+        #Pour un équilibrage 50/50
         pairs_using_numbers = [(-1,text)  for (target,text) in pairs  if target == 'Negative']
         Positive_reviews =  [(1,text) for (target,text) in pairs if target == 'Positive']
         pairs_using_numbers += Positive_reviews[:int(len(pairs_using_numbers))]
@@ -220,7 +241,7 @@ def folder2data(train_filename,test_filename,balanced_tr ,balanced_te, n_feature
         """
     else :
         pairs_using_numbers = [(1,text) for (target,text) in pairs if target == 'Positive']
-        pairs_using_numbers += [(-1,text)  for (target,text) in pairs  if target == 'Negative']
+        pairs_using_numbers += [(-1,text)  for (target,text) in pairs  if (target == 'Negative' or target == 'Neutral')]
         tr_pairs = pairs_using_numbers
 
     pairs = getData(test_filename)
@@ -228,14 +249,14 @@ def folder2data(train_filename,test_filename,balanced_tr ,balanced_te, n_feature
     print(pairs[:2])
 
     if balanced_te :
-        pairs_using_numbers = [(-1,text)  for (target,text) in pairs  if target == 'Negative']
+        pairs_using_numbers = [(-1,text)  for (target,text) in pairs  if (target == 'Negative' or target == 'Neutral')]
         Positive_reviews =  [(1,text) for (target,text) in pairs if target == 'Positive']
         pairs_using_numbers += Positive_reviews[:int(len(pairs_using_numbers))]
         te_pairs = pairs_using_numbers
 
     else :
         pairs_using_numbers = [(1,text) for (target,text) in pairs if target == 'Positive']
-        pairs_using_numbers += [(-1,text)  for (target,text) in pairs  if target == 'Negative']
+        pairs_using_numbers += [(-1,text)  for (target,text) in pairs  if (target == 'Negative' or target == 'Neutral')]
         te_pairs = pairs_using_numbers
 
 
@@ -257,16 +278,7 @@ def folder2data(train_filename,test_filename,balanced_tr ,balanced_te, n_feature
     X_tr_reduced_dim = truncatedsvd.transform(X_tr_token)
     X_te_reduced_dim = truncatedsvd.transform(X_te_token)
 
-    """
-    new_tr_pairs = []
-    for i in range(len(tr_pairs)):
-        (note,_) = tr_pairs[i]
-        note = Variable(torch.FloatTensor([[note]]))
 
-        vect = X_tr_reduced_dim[i,:]
-        variable_vect = torch.autograd.Variable(torch.Tensor(vect))
-        new_tr_pairs.append(note,variable_vect)
-"""
     new_tr_pairs = [Variable(torch.FloatTensor(X_tr_reduced_dim)),Variable(torch.FloatTensor([[note for (note,_) in tr_pairs]]))]
 
 
@@ -292,7 +304,7 @@ def folder2data(train_filename,test_filename,balanced_tr ,balanced_te, n_feature
 
 
 # ==================================================================
-# ================ Using the RNN in itself =========================
+# ================ Using the MLP in itself =========================
 # ==================================================================
 
 training_set_folder = "../../data/data_books_training_set"
@@ -301,10 +313,10 @@ test_set_folder = "../../data/data_books_testing_set"
 
 
 
-n_features=150
-tr_pairs,te_pairs = folder2data(training_set_folder,test_set_folder,balanced_tr = True,balanced_te = False,n_features=n_features)
+n_features=10
+tr_pairs,te_pairs = folder2data(training_set_folder,test_set_folder,balanced_tr = True,balanced_te = True,n_features=n_features)
 
-hidden_size = 75
+hidden_size = 5
 batch_size = tr_pairs[0].data.size()[0]
 
 RNN = my_RNN(n_features, hidden_size, batch_size, n_layers = 1)
@@ -312,95 +324,17 @@ RNN = my_RNN(n_features, hidden_size, batch_size, n_layers = 1)
 
 
 lr = 0.005
-N_epochs = 30000
+N_epochs = 3
 print("learning rate",lr)
 print("batch_size",batch_size)
-RNN.trainIters( N_epochs,tr_pairs,lr,500,5000)
+RNN.trainIters( N_epochs,tr_pairs,te_pairs,lr,500,5000)
 
 
 RNN.evaluateRandomly(te_pairs) # show global results
 
-torch.save(RNN,'MLP_75')
+torch.save(RNN,'MLP')
 #cours ; cd 2eme_partie_S9/Transfer_learning/TransferLearningProject/learning/ ; python MLP_base.py
 
-
-
-"""
-MLP = torch.load('MLP_singleset_data_videos_training_set_unbalanced_data_videos_testing_set')
-
-
-train_filename = "../../data/data_videos_training_set"
-test_filename = "../../data/data_videos_testing_set"
-balanced = True
-n_features=200
-
-tr_te_pairs = {}
-for filename in [train_filename,test_filename]:
-    pairs = getData(filename)
-
-    print(pairs[:2])
-
-
-    if balanced :
-        pairs_using_numbers = [(-1,text)  for (target,text) in pairs  if target == 'Negative']
-        Positive_reviews =  [(1,text) for (target,text) in pairs if target == 'Positive']
-        pairs_using_numbers += Positive_reviews[:int(len(pairs_using_numbers))]
-        pairs = pairs_using_numbers
-    else :
-        pairs_using_numbers = [(1,text) for (target,text) in pairs if target == 'Positive']
-        pairs_using_numbers += [(-1,text)  for (target,text) in pairs  if target == 'Negative']
-        pairs = pairs_using_numbers
-
-    tr_te_pairs[filename] = pairs
-
-tr_pairs,te_pairs = tr_te_pairs[train_filename],tr_te_pairs[test_filename]
-
-
-tfidf_vectorizer = TfidfVectorizer(ngram_range=(1,2))
-tfidf_vectorizer.fit([ text for (_,text) in tr_pairs+te_pairs])
-
-# fitting
-X_tr_token = tfidf_vectorizer.transform([ text for (_,text) in tr_pairs])
-X_te_token = tfidf_vectorizer.transform([ text for (_,text) in te_pairs])
-
-truncatedsvd = TruncatedSVD(n_components=n_features) # prépare à projeter les données dans un espace à n_components dimensions
-truncatedsvd.fit(X_tr_token)
-truncatedsvd.fit(X_te_token)
-
-
-# Réduction de dimension
-X_tr_reduced_dim = truncatedsvd.transform(X_tr_token)
-X_te_reduced_dim = truncatedsvd.transform(X_te_token)
-
-new_tr_pairs = [Variable(torch.FloatTensor(X_tr_reduced_dim)),Variable(torch.FloatTensor([[note for (note,_) in tr_pairs]]))]
-
-test_set_folder = "../../data/data_videos_cleaned/"
-pairs = getData(test_set_folder)
-
-if balanced :
-    pairs_using_numbers = [(-1,text)  for (target,text) in pairs  if target == 'Negative']
-    Positive_reviews =  [(1,text) for (target,text) in pairs if target == 'Positive']
-    pairs_using_numbers += Positive_reviews[:int(len(pairs_using_numbers))]
-    te_pairs = pairs_using_numbers
-else :
-    pairs_using_numbers = [(1,text) for (target,text) in pairs if target == 'Positive']
-    pairs_using_numbers += [(-1,text)  for (target,text) in pairs  if target == 'Negative']
-    te_pairs = pairs_using_numbers
-
-X_te_token = tfidf_vectorizer.transform([ text for (_,text) in te_pairs])
-X_te_reduced_dim = truncatedsvd.transform(X_te_token)
-
-new_te_pairs = []
-for i in range(len(te_pairs)):
-    (note,_) = te_pairs[i]
-    note = Variable(torch.FloatTensor([[note]]))
-
-    vect = X_te_reduced_dim[i,:]
-    variable_vect = torch.autograd.Variable(torch.Tensor(vect))
-    new_te_pairs.append((note,variable_vect))
-
-MLP.evaluateRandomly(new_te_pairs) # show global results
-"""
 
 print('')
 print('')
